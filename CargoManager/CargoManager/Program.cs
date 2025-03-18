17cd2d38f3d598615ac5ae538d1cc3229253dd30
Arina Liubas <arinaliubas@gmail.com>
2025-03-18 13:34:50 +0100

    Added Main method

--- CargoManager/CargoManager/Program.cs
+++ CargoManager/CargoManager/Program.cs
@@ -1,10 +1,27 @@
-﻿// See https://aka.ms/new-console-template for more information
+﻿
+using CargoManager;
 
 class Program
-{
-    static void Main(string[] args)
+{ 
+    static void Main()
     {
-        var ship = new Ship(5, 50000);
+        Ship ship = new Ship(5, 500, 30);
         
+        LiquidContainer liquidContainer = new LiquidContainer(150, 8, 4, 40, true);
+        GasContainer gasContainer = new GasContainer(200, 10, 5, 50, 10);
+        RefrigeratedContainer refrigeratedContainer = new RefrigeratedContainer(100, 6, 3, 30, "Fish", 2);
+        
+        ship.AddContainer(liquidContainer);
+        ship.AddContainer(gasContainer);
+        ship.AddContainer(refrigeratedContainer);
+
+        liquidContainer.LoadCargo(70);
+        gasContainer.LoadCargo(50);
+        refrigeratedContainer.LoadCargo(40);
+        
+        Console.WriteLine(ship);
+        Console.WriteLine(liquidContainer);
+        Console.WriteLine(gasContainer);
+        Console.WriteLine(refrigeratedContainer);
     }
 }
