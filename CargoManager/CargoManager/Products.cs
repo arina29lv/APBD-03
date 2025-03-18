0c8e56d02fd4dedd2d7e4a9a906925c16dd30ebe
Arina Liubas <arinaliubas@gmail.com>
2025-03-18 12:23:57 +0100

    Added Products Dictionary for RefrigeratedContainer class

---
+++ CargoManager/CargoManager/Products.cs
@@ -0,0 +1,18 @@
+namespace CargoManager;
+
+public static class PossibleProducts
+{
+    public static readonly Dictionary<string, double> Products = new()
+    {
+        ["Bananas"] = 13.3,
+        ["Chocolate"] = 18,
+        ["Fish"] = 2,
+        ["Meat"] = 15,
+        ["Ice Cream"] = -18,
+        ["Frozen Pizza"] = 30,
+        ["Cheese"] = 7.2,
+        ["Sausages"] = 5,
+        ["Butter"] = 20.5,
+        ["Eggs"] = 19
+    };
+}
--- CargoManager/CargoManager/RefrigeratedContainer.cs
+++ CargoManager/CargoManager/RefrigeratedContainer.cs
@@ -9,8 +9,18 @@ public class RefrigeratedContainer : Container
         double temperature)
         : base("Refrigerated", 0, maxLoad, height, depth, tareWeight)
     {
+        if(!PossibleProducts.Products.ContainsKey(productType))
+            throw new ArgumentException($"Product '{productType}' is not allowed in refrigerated container.");
+        
+        if(temperature < PossibleProducts.Products[ProductType])
+            throw new ArgumentException($"Temperature {temperature} is too low for {ProductType}. Allowed: {PossibleProducts.Products[ProductType]} degrees.");
+        
         ProductType = productType;
         Temperature = temperature;
     }
-        
+
+    public override string ToString()
+    {
+        return base.ToString() + $", Product: {ProductType}, Temperature: {Temperature} degrees.";
+    }
 }
