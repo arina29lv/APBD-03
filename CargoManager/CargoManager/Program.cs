2b21b975caea0dd9d3b51a0bd2478bce87594886
Arina Liubas <arinaliubas@gmail.com>
2025-03-18 13:14:39 +0100

    Improved Ship class

--- CargoManager/CargoManager/Program.cs
+++ CargoManager/CargoManager/Program.cs
@@ -1,3 +1,10 @@
 ﻿// See https://aka.ms/new-console-template for more information
 
-Console.WriteLine("Hello, World!");
+class Program
+{
+    static void Main(string[] args)
+    {
+        var ship = new Ship(5, 50000);
+        
+    }
+}
--- CargoManager/CargoManager/Ship.cs
+++ CargoManager/CargoManager/Ship.cs
@@ -3,11 +3,13 @@ namespace CargoManager;
 public class Ship
 {
     private List<Container> containers = new();
+    public double MaxSpeed { get; set; }
     public int MaxContainers { get; set; }
     public double MaxWeight { get; set; }
 
-    public Ship(int maxContainers, int maxWeight)
+    public Ship(double maxSpeed, int maxContainers, int maxWeight)
     {
+        MaxSpeed = maxSpeed;
         MaxContainers = maxContainers;
         MaxWeight = maxWeight;
     }
@@ -16,11 +18,26 @@ public class Ship
     {
         if(containers.Count >= MaxContainers)
             throw new InvalidOperationException("No space to add more containers.");
+        if (containers.Sum(c => c.TareWeight + c.CurrentLoad)
+            + container.TareWeight + container.CurrentLoad > MaxWeight)
+            throw new InvalidOperationException("Exceeding ships weight limit.");
         containers.Add(container);
+    }
+
+    public void RemoveContainer(Container container)
+    {
+        containers.Remove(container);
+    }
+
+    public void TransferContainer(Ship targedShip, Container container)
+    {
+        RemoveContainer(container);
+        targedShip.AddContainer(container);
     }
 
     public override string ToString()
     {
-        return $"Ship: {containers.Count} / {MaxContainers}";
+        double totslWeight = containers.Sum(c => c.TareWeight + c.CurrentLoad);
+        return $"SHIP: {containers.Count}/{MaxContainers} containers, {totslWeight}/{MaxWeight} tons; Speed: {MaxSpeed} knots.";
     }
 }
